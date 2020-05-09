84e6442a1618a0421bf1e209767d66a55273fb7a
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-05-09 12:36:49 +0300

    Create kick.py
    This is module to kick users from the server.

---
+++ discordbot/cogs/kick.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+from discordbot import Module
+
+class Kick(Module):
+	'''Kicks users from the server.'''
+
+	@commands.command(usage='<member> <reason>')
+	async def kick(self, ctx: commands.Context, member: discord.Member = None, *, reason: str = None) -> None:
+		'''Kicks user from the server.'''
+		if reason is not None and member is not None:
+			await member.kick(reason=reason)
+
+def setup(bot: commands.Bot) -> None:
+	bot.add_cog(Kick(bot))
