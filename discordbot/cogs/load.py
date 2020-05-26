4521d0f72a52d714a1a20c5ca300c90b998d7d13
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-05-26 22:33:23 +0300

    Update load.py

--- discordbot/cogs/load.py
+++ discordbot/cogs/load.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
-from discordbot import Module
-from discordbot import modules_load, modules_remove
+from discordbot.utils import Module
 
 class Load(Module):
 	'''Loads, unloads and reloads packages.'''
@@ -9,40 +8,36 @@ class Load(Module):
 	@commands.command(usage='<package>')
 	async def load(self, ctx: commands.Context, extension: str = None) -> None:
 		'''Loads new package.'''
-		if extension is not None:
-			if not extension == 'all':
-				try:
-					self.bot.load_extension("discordbot.cogs.{}".format(extension))
-				except commands.ExtensionError as e:
-					await ctx.send(e.__str__().replace('discordbot.cogs.', ''))
-			else:
-				modules_load(self.bot)
+		if extension is not None and not extension == 'all':
+			try:
+				self.bot.load_extension("discordbot.cogs.{}".format(extension))
+			except commands.ExtensionError as e:
+				await ctx.send(e.__str__().replace('discordbot.cogs.', ''))
+		else:
+			self.bot.modules.load()
 
 	@commands.command(usage='<package>')
 	async def reload(self, ctx, extension: str = None) -> None:
 		'''Reloads package.'''
-		if extension is not None:
-			if not extension == 'all':
-				try:
-					self.bot.unload_extension("discordbot.cogs.{}".format(extension))
-					self.bot.load_extension("discordbot.cogs.{}".format(extension))
-				except commands.ExtensionError as e:
-					await ctx.send(e.__str__().replace('discordbot.cogs.', ''))
-			else:
-				modules_remove(self.bot)
-				modules_load(self.bot)
+		if extension is not None and not extension == 'all':
+			try:
+				self.bot.unload_extension("discordbot.cogs.{}".format(extension))
+				self.bot.load_extension("discordbot.cogs.{}".format(extension))
+			except commands.ExtensionError as e:
+				await ctx.send(e.__str__().replace('discordbot.cogs.', ''))
+		else:
+			self.bot.modules.reload()
 
 	@commands.command(usage='<package>')
 	async def unload(self, ctx, extension: str = None):
 		'''Remove package'''
-		if extension is not None:
-			if not extension == 'all':
-				try:
-					self.bot.unload_extension("discordbot.cogs.{}".format(extension))
-				except commands.ExtensionError as e:
-					await ctx.send(e.__str__().replace('discordbot.cogs.', ''))
-			else:
-				modules_remove(self.bot)
+		if extension is not None and not extension == 'all':
+			try:
+				self.bot.unload_extension("discordbot.cogs.{}".format(extension))
+			except commands.ExtensionError as e:
+				await ctx.send(e.__str__().replace('discordbot.cogs.', ''))
+		else:
+			self.bot.modules.unload()
 
 def setup(bot: commands.Bot) -> None:
 	bot.add_cog(Load(bot))
