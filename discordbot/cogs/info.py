e97fe7f2d37563bc0d02cf8ea03ecd6712d8d3c8
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-05-26 22:32:38 +0300

    Create info.py

---
+++ discordbot/cogs/info.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+from discordbot.utils import Module
+
+class Info(Module):
+	'''Shows information about the user.'''
+
+	@commands.command(usage='<member>')
+	async def info(self, ctx: commands.Context, member: discord.Member = None) -> None:
+		'''Prints how much time does user spent in voice chats.'''
+		if member is not None:
+			await ctx.send(str(self.bot.members[member.guild.name][member.display_name]))
+
+def setup(bot: commands.Bot) -> None:
+	bot.add_cog(Info(bot))
