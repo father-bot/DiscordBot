c69230155aee3d4f4cd0b295727cd9812e82d21b
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-05-30 13:34:29 +0100

    Update ban.py

--- discordbot/cogs/ban.py
+++ discordbot/cogs/ban.py
@@ -11,22 +11,20 @@ class Ban(Module):
 	async def report(self, ctx: commands.Context, member: discord.Member = None, *, reason: str = None) -> None:
 		'''Reports user.'''
 		if reason is not None and member is not None: # If member or reason isn't None
-			__member__ = self.bot.members[member.guild.name][member.name]
-			__member__.json.config_to_member(__member__)
-			if __member__.warnings >= 2:
+			__member = self.bot.members
+			warnings = __member.get_member_warnings(member.guild.name, member.id)
+			if warnings >= 2:
 				await ctx.send('Member {} was banned.'.format(member.name))
-				await member.ban(reason=reason) # Bans member
+				await member.ban(reason=reason)
 			else:
 				mute_role = discord.utils.get(ctx.message.guild.roles, name='mute')
-				__member__.warnings += 1
-				__member__.json.update_config(member.name, member.guild.name)
-				if 3-__member__.warnings == 1:
-					await ctx.send('1 more report for ban.')
-				else:
-					await ctx.send('{} more reports for ban.'.format(3-__member__.warnings))
+				__member.update_member_warnings(member.guild.name, member.id)
+				warnings = __member.get_member_warnings(member.guild.name, member.id)
+				self.bot.db.set_new_database_value(member.guild.name, member.id, __member.get_info(member.guild.name, member.id)[1:])
+				await ctx.send('{} more report{} for ban.'.format(3-warnings, 's' if 3-warnings != 1 else ''))
 				await member.add_roles(mute_role)
-				await asyncio.sleep(2 * 60 * 60) # 2 hours
-				if member.name in self.bot.members[member.guild.name]: # If member wasn't banned.
+				await asyncio.sleep(60)
+				if member.name in __member.get_members().keys():
 					await member.remove_roles(mute_role)
 
 	@commands.command(usage='<member> <reason>')
