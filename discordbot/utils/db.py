479dcbc2b18d0c2a1459d98962dec28ec5ef0065
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-06-28 21:27:39 +0100

    Update db.py

--- discordbot/utils/db.py
+++ discordbot/utils/db.py
@@ -1,91 +1,240 @@
-import sqlite3
-from sqlite3 import Error
+# -*- coding: utf-8 -*-
+
+"""
+MIT License
+
+Copyright (c) 2020 Simon Chaykin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import os
+import sqlite3
+from discord.ext import commands
 
-class Database(object):
-	def __init__(self, bot):
-		self.bot = bot
-		self.database = self.bot.database
-		self.conn = None
-		self.c = None
+class SQLiteDatabase:
+    """The class of sqlite database utilities that require the :class:`commands.Bot`.
+    to be passed to be useful.
+    """
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.database = './discordbot/data/database/'
+        self.database_tables = {}
+        self.conn = None
+        self.cursor = None
 
-	def create_database(self, name):
-		if not os.path.exists(f'{self.database}/{name}.db'):
-			f = open(f'{self.database}/{name}.db', 'w')
-			f.close()
+    def create_database(self, name: str):
+        """Create new database.
 
-	def create_databases(self):
-		for guild in self.bot.guilds:
-			self.create_database(guild.name)
-	
-	def exists_database(self, name):
-		return os.path.exists(f'{self.database}/{name}.db')
+        Parameter
+        ---------
+        name: :class:`str`
+            Name of new database.
+        """
+        if not os.path.exists(f'{self.database}/{name}.db'):
+            filename = open(f'{self.database}/{name}.db', 'w')
+            filename.close()
 
-	def create_default_database(self, name):
-		self.conn = sqlite3.connect(f'{self.database}/{name}.db')
-		self.c = self.conn.cursor()
-		self.c.execute('''CREATE TABLE Members (
-							id INTEGER PRIMARY KEY,
-							nick text,
-							server text,
-							time int,
-							warnings int,
-							partner text
-						)''')
-		self.conn.commit()
-		self.conn.close()
-	
-	def set_default_database_value(self, name, data):
-		self.conn = sqlite3.connect(f'{self.database}/{name}.db')
-		self.c = self.conn.cursor()
-		#print(data[0])
-		self.c.execute('''INSERT INTO Members VALUES ({}, '{}', '{}', {}, {}, '{}')'''.format(data[0], data[1], data[2], data[3], data[4], data[5]))
-		self.conn.commit()
-		self.conn.close()
-	
-	def delete_database_id(self, name, id):
-		self.conn = sqlite3.connect(f'{self.database}/{name}.db')
-		self.c = self.conn.cursor()
-		self.c.execute('''DELETE FROM Members WHERE id = {}'''.format(id))
-		self.conn.commit()
-		self.conn.close()
+    def create_databases(self):
+        """Create new databases.
+        """
+        for guild in self.bot.guilds:
+            self.create_database(guild.name)
 
-	def set_new_database_value(self, name, id, data):
-		self.conn = sqlite3.connect(f'{self.database}/{name}.db')
-		#print(name, id, data)
-		self.c = self.conn.cursor()
-		self.c.execute('''UPDATE Members
-						  SET
-							nick = '{}',
-							server = '{}',
-							time = {},
-							warnings = {},
-							partner = '{}'
-						  WHERE id = {}
-						  '''.format(data[0], data[1], data[2], data[3], data[4], id))
-		self.conn.commit()
-		self.conn.close()
+    def exists_database(self, name: str):
+        """Show if database exists.
 
-	def get_database_value(self, name, id):
-		self.conn = sqlite3.connect(f'{self.database}/{name}.db')
-		self.c = self.conn.cursor()
-		self.c.execute('SELECT * FROM Members WHERE id = {}'.format(id))
-		data = self.c.fetchall()[0]
-		self.conn.commit()
-		self.conn.close()
-		return data
+        Parameter
+        ---------
+        name: :class:`str`
+            Name of the database.
+        """
+        return os.path.exists(f'{self.database}/{name}.db')
 
-	def get_database_values(self, name):
-		self.conn = sqlite3.connect(f'{self.database}/{name}.db')
-		self.c = self.conn.cursor()
-		self.c.execute('SELECT * FROM Members')
-		data = self.c.fetchall()
-		self.conn.commit()
-		self.conn.close()
-		return data
+    def create_new_table_database(self, name: str, table_name: str, options: dict):
+        """Create new database table.
 
-	def is_default(self, name, id):
-		data = self.get_database_value(name, id)
-		if data[3:] == (0, 0, ''):
-			return True
-		return False
+        Parameter
+        ---------
+        name: :class:`str`
+            Name of the database.
+        table_name: :class:`str`
+            Name of the table.
+        options: :class:`dict`
+            Options for the table.
+        """
+        if not table_name in self.database_tables.keys():
+            try:
+                self.database_tables[table_name] = [{key:options[key]} for key in options]
+                table = ',\n'.join([f'{key} {options[key]}' for key in options]) if options else ''
+                self.conn = sqlite3.connect(f'{self.database}/{name}.db')
+                self.cursor = self.conn.cursor()
+                self.cursor.execute('''CREATE TABLE {} (
+                                       id INTEGER PRIMARY KEY,
+                                       {}
+                                    )'''.format(table_name, table))
+                self.conn.commit()
+                self.conn.close()
+            except sqlite3.OperationalError:
+                return
+
+    def _to_string(self, table_name: str, data: list):
+        """Converts data to string.
+
+        Parameters
+        ----------
+        table_name: :class:`str`
+            Name of the table.
+        data: :class:`list`
+            All the data.
+        """
+        result = ''
+        for index, value in enumerate(data, 0):
+            table = self.database_tables[table_name][index]
+            key = list(table.keys())[0]
+            if table[key] == 'text':
+                result += f'"{value}", '
+            elif table[key] == 'int':
+                result += f'{value}, '
+        return result[:-2]
+
+    def _to_string_with_variables(self, table_name: str, data: list):
+        """Converts data to string with variables name.
+
+        Parameters
+        ----------
+        table_name: :class:`str`
+            Name of the table.
+        data: :class:`list`
+            All the data.
+        """
+        result = ''
+        for index, value in enumerate(data, 0):
+            table = self.database_tables[table_name][index]
+            key = list(table.keys())[0]
+            if table[key] == 'text':
+                result += f'{key} = "{value}", '
+            elif table[key] == 'int':
+                result += f'{key} = {value}, '
+        return result[:-2]
+
+    def set_default_database_value(self, name: str, table_name: str, data: list):
+        """Set default database values.
+
+        Parameters
+        ----------
+        name: :class:`str`
+            Name of the database.
+        table_name: :class:`str`
+            Name of the table.
+        data: :class:`list`
+            All the data.
+        """
+        if table_name in self.database_tables.keys():
+            self.conn = sqlite3.connect(f'{self.database}/{name}.db')
+            self.cursor = self.conn.cursor()
+            try:
+                self.cursor.execute('''INSERT INTO {} VALUES ({}, {})'''.format(
+                    table_name, data[0], self._to_string(table_name, data[1:])))
+            except sqlite3.IntegrityError:
+                pass
+            self.conn.commit()
+            self.conn.close()
+
+    def delete_database_id(self, name: str, table_name: str, identifier: int):
+        """Converts data to string with variables name.
+
+        Parameters
+        ----------
+        name: :class:`str`
+            Name of the database.
+        table_name: :class:`str`
+            Name of the table.
+        identifier: :class:`int`
+            ID of section.
+        """
+        if table_name in self.database_tables.keys():
+            self.conn = sqlite3.connect(f'{self.database}/{name}.db')
+            self.cursor = self.conn.cursor()
+            self.cursor.execute('''DELETE FROM {} WHERE id = {}'''.format(table_name, identifier))
+            self.conn.commit()
+            self.conn.close()
+
+    def set_new_database_value(self, name: str, table_name: str, identifier: int, data: list):
+        """Set new database values.
+
+        Parameters
+        ----------
+        name: :class:`str`
+            Name of the database.
+        table_name: :class:`str`
+            Name of the table.
+        identifier: :class:`int`:
+            ID of section.
+        data: :class:`list`
+            All the data.
+        """
+        if table_name in self.database_tables.keys():
+            self.conn = sqlite3.connect(f'{self.database}/{name}.db')
+            self.cursor = self.conn.cursor()
+            result = self._to_string_with_variables(table_name, data)
+            self.cursor.execute('''UPDATE {}
+                                   SET
+                                     {}
+                                   WHERE id = {}'''.format(table_name, result, identifier))
+            self.conn.commit()
+            self.conn.close()
+
+    def get_database_value(self, name: str, table_name: str, identifier: int):
+        """Get database value using ID.
+
+        Parameters
+        ----------
+        name: :class:`str`
+            Name of the database.
+        table_name: :class:`str`
+            Name of the table.
+        identifier: :class:`int`
+            ID of section.
+        """
+        self.conn = sqlite3.connect(f'{self.database}/{name}.db')
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('SELECT * FROM {} WHERE id = {}'.format(table_name, identifier))
+        data = self.cursor.fetchall()[0]
+        self.conn.commit()
+        self.conn.close()
+        return data
+
+    def get_database_values(self, name: str, table_name: str):
+        """Get database values.
+
+        Parameters
+        ----------
+        name: :class:`str`
+            Name of the database.
+        table_name: :class:`str`
+            Name of the table.
+        """
+        self.conn = sqlite3.connect(f'{self.database}/{name}.db')
+        self.cursor = self.conn.cursor()
+        self.cursor.execute(f'SELECT * FROM {table_name}')
+        data = self.cursor.fetchall()
+        self.conn.commit()
+        self.conn.close()
+        return data
